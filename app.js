989c806a5ee2a4269699c33f41c1ef4cac8835fa
Emmanuel Meinike <emmanuel.meinike@gmail.com>
2022-08-22 18:11:26 +0200

    add history

--- app.js
+++ app.js
@@ -10,8 +10,13 @@ const imageAspectRatioContainer = document.querySelector(
 const resolutionXInput = document.querySelector('.resolution-x');
 const resolutionYInput = document.querySelector('.resolution-y');
 const resetButton = document.querySelector('.reset-button');
+const historyButton = document.querySelector('.history-button');
 const colorSchemeButton = document.querySelector('.color-scheme-switch');
 const notificationElement = document.querySelector('.notification');
+const overlay = document.querySelector('.overlay');
+const overlayHeadline = document.querySelector('.overlay-headline');
+const overlayContent = document.querySelector('.overlay-content');
+const closeOverlayButton = document.querySelector('.close-overlay-button');
 
 const resolutionXGeneratedInput = document.querySelector(
     '.resolution-x-generated'
@@ -33,7 +38,14 @@ const COLOR_SCHEME_MAP = {
 };
 const DEFAULT_COLOR_SCHEME = COLOR_SCHEME_MAP.dark.name;
 const NOTIFICATION_SHOW_TIME = 2000;
+const RESOLUTION_PERSIS_TIME = 2000;
 let notificationTimeout = null;
+let inputResolutionChangeTimeout = null;
+
+const StorageProvider = {
+    get: (key) => window.localStorage.getItem(key),
+    set: (key, value) => window.localStorage.setItem(key, value),
+};
 
 const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
 
@@ -57,6 +69,33 @@ const updateUI = (width, height, resolutionElement, aspectRatioElement) => {
     resolutionYGeneratedInput.value = '';
 };
 
+const loadResolutionHistory = () => {
+    const historyString = StorageProvider.get('ARC:resolutionHistory');
+    if (!historyString) {
+        return [];
+    }
+
+    try {
+        const history = JSON.parse(historyString);
+        return history;
+    } catch (e) {
+        console.log(e);
+        return [];
+    }
+};
+
+const addResolutionToHistory = (width, height) => {
+    const history = loadResolutionHistory();
+    const entry = { width, height, key: width + 'x' + height };
+    StorageProvider.set(
+        'ARC:resolutionHistory',
+        JSON.stringify([
+            entry,
+            ...history.filter((item) => item.key !== entry.key),
+        ])
+    );
+};
+
 const init = () => {
     resolutionXInput.value = DEFAULT_WIDTH;
     resolutionYInput.value = DEFAULT_HEIGHT;
@@ -72,10 +111,10 @@ const init = () => {
 };
 
 const getCurrentColorSchemeMode = () =>
-    window.localStorage.getItem('ARC:colorSchmeMode') || DEFAULT_COLOR_SCHEME;
+    StorageProvider.get('ARC:colorSchmeMode') || DEFAULT_COLOR_SCHEME;
 
 const setCurrentColorSchemeMode = (value) => {
-    window.localStorage.setItem('ARC:colorSchmeMode', value);
+    StorageProvider.set('ARC:colorSchmeMode', value);
     root.classList.remove(
         ...[COLOR_SCHEME_MAP.dark.className, COLOR_SCHEME_MAP.light.className]
     );
@@ -118,6 +157,14 @@ resolutionXInput.addEventListener('input', (e) => {
     const width = Number(value);
     const height = Number(resolutionYInput.value);
     updateUI(width, height, imageResolution, imageAspectRatio);
+    clearTimeout(inputResolutionChangeTimeout);
+    inputResolutionChangeTimeout = setTimeout(() => {
+        console.log(
+            Number(resolutionXInput.value),
+            Number(resolutionYInput.value)
+        );
+        addResolutionToHistory(width, height);
+    }, RESOLUTION_PERSIS_TIME);
 });
 
 resolutionYInput.addEventListener('input', (e) => {
@@ -125,6 +172,14 @@ resolutionYInput.addEventListener('input', (e) => {
     const height = Number(value);
     const width = Number(resolutionXInput.value);
     updateUI(width, height, imageResolution, imageAspectRatio);
+    clearTimeout(inputResolutionChangeTimeout);
+    inputResolutionChangeTimeout = setTimeout(() => {
+        console.log(
+            Number(resolutionXInput.value),
+            Number(resolutionYInput.value)
+        );
+        addResolutionToHistory(width, height);
+    }, RESOLUTION_PERSIS_TIME);
 });
 
 [
@@ -162,6 +217,40 @@ resolutionYGeneratedInput.addEventListener('input', (e) => {
 });
 
 resetButton.addEventListener('click', () => init());
+
+overlay.addEventListener('click', (e) => {
+    if (e.target === overlay) {
+        overlay.classList.add('hide');
+    }
+});
+
+historyButton.addEventListener('click', () => {
+    const history = loadResolutionHistory();
+    overlay.classList.remove('hide');
+    const historyList = document.createElement('ul');
+    historyList.classList.add('history-list');
+    history.forEach((entry) => {
+        const li = document.createElement('li');
+        li.textContent = `${entry.width} x ${entry.height}`;
+        li.addEventListener('click', () => {
+            resolutionXInput.value = entry.width;
+            resolutionYInput.value = entry.height;
+            updateUI(
+                entry.width,
+                entry.height,
+                imageResolution,
+                imageAspectRatio
+            );
+            overlay.classList.add('hide');
+        });
+        historyList.appendChild(li);
+    });
+    overlayContent.replaceChildren(historyList);
+});
+
+closeOverlayButton.addEventListener('click', () =>
+    overlay.classList.add('hide')
+);
 
 imageResolution.addEventListener('click', (e) => {
     copyToClipboard(e.target.textContent);
